5f513a5e5b3ff878efd5392c17f82bdba1c4501f
Mateusz Skrobis <skrobis.mateusz@gmail.com>
2017-08-07 15:20:58 +0200

    Add product model

---
+++ mirage/factories/product.js
@@ -0,0 +1,23 @@
+import { Factory, faker } from 'ember-cli-mirage';
+
+export default Factory.extend({
+  name() {
+    return faker.commerce.productName();
+  },
+  description() {
+    return faker.lorem.sentence();
+  },
+  sku() {
+    return faker.random.number();
+  },
+  availability() {
+    return faker.random.boolean();
+  },
+  price() {
+    return faker.commerce.price();
+  },
+  currency: 'EUR',
+  quantity() {
+    return faker.random.number();
+  }
+});
