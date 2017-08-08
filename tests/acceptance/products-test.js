33f44949f882345ec90ee1659d10f5822cfe61f5
Mateusz Skrobis <skrobis.mateusz@gmail.com>
2017-08-08 11:13:12 +0200

    Implement product-thumb component

---
+++ tests/acceptance/products-test.js
@@ -0,0 +1,26 @@
+import { describe, it, beforeEach, afterEach } from 'mocha';
+import { expect } from 'chai';
+import startApp from 'bsc-ember/tests/helpers/start-app';
+import destroyApp from 'bsc-ember/tests/helpers/destroy-app';
+import { visit, find } from 'ember-native-dom-helpers';
+
+describe('Acceptance | products', function() {
+  this.timeout(0);
+  let application;
+
+  beforeEach(function() {
+    application = startApp();
+  });
+
+  afterEach(function() {
+    destroyApp(application);
+  });
+
+  it('can visit products page and go to product detail page', async function() {
+    server.create('product', { name: 'Personal Computer' });
+    await visit('/products');
+    await click('[data-test-product-show-link]');
+    expect(find('[data-test-product-page-name]').textContent)
+      .to.include('Personal Computer');
+  });
+});
