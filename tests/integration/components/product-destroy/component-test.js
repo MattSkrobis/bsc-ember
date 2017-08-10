5574e560c9c85dc041cc19b8f11bb035184b9468
Mateusz Skrobis <skrobis.mateusz@gmail.com>
2017-08-10 14:36:32 +0200

    Refactoring of spec, make scope of each test smaller

--- tests/integration/components/product-destroy/component-test.js
+++ tests/integration/components/product-destroy/component-test.js
@@ -15,18 +15,35 @@ describe('Integration | Component | product destroy', function() {
     this.set('destroy', sinon.spy());
     this.render(hbs`
     {{#product-destroy onConfirm=(action destroy product)}}
-    Remove
+      Remove
     {{/product-destroy}}
     `);
     // renders component
     expect(find('[data-test-product-destroy-button]')).to.have.exist;
+  });
 
-    // renders confirmation text
-    click('[data-test-product-destroy-button]');
-
+  it('click cancel on confirmation', function() {
+    this.set('product', { name: 'Phone', description: 'Black', id: 1 });
+    this.set('destroy', sinon.spy());
+    this.render(hbs`
+    {{#product-destroy onConfirm=(action destroy product)}}
+      Remove
+    {{/product-destroy}}
+    `);
     // click cancel hides buttons
+    click('[data-test-product-destroy-button]');
     click('[data-test-product-destroy-cancel-button]');
     expect(find('[data-test-product-destroy-cancel-button]')).to.not.have.exist;
+  });
+
+  it('click confirm on confirmation', function() {
+    this.set('product', { name: 'Phone', description: 'Black', id: 1 });
+    this.set('destroy', sinon.spy());
+    this.render(hbs`
+    {{#product-destroy onConfirm=(action destroy product)}}
+      Remove
+    {{/product-destroy}}
+    `);
 
     // click confirm triggers action
     click('[data-test-product-destroy-button]');
