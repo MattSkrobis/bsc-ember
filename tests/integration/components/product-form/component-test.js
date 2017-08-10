61aeffd4b2525166c71186bf864ea95f64668d86
Mateusz Skrobis <skrobis.mateusz@gmail.com>
2017-08-10 09:55:38 +0200

    Refactor product edit new components

--- tests/integration/components/product-form/component-test.js
+++ tests/integration/components/product-form/component-test.js
@@ -4,14 +4,14 @@ import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 import { find } from 'ember-native-dom-helpers';
 
-describe('Integration | Component | product edit', function() {
-  setupComponentTest('product-edit', {
+describe('Integration | Component | product form', function() {
+  setupComponentTest('product-form', {
     integration: true
   });
 
   it('renders', function() {
     this.set('product', { name: 'Phone', description: 'Black', sku: 1 });
-    this.render(hbs`{{product-edit product=product}}`);
+    this.render(hbs`{{product-form product=product}}`);
     expect(find('[data-test-product-form-name]').value).to.include('Phone');
   });
 });
