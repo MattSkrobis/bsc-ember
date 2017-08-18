77b98d5d7205d0423f6970cac14e732c5f9e96bf
Mateusz Skrobis <skrobis.mateusz@gmail.com>
2017-08-18 13:26:42 +0200

    Add acceptance test, refactoring

---
+++ tests/pages/category.js
@@ -0,0 +1,28 @@
+import {
+  create,
+  collection,
+  clickable,
+  text,
+  visitable,
+  fillable } from 'ember-cli-page-object';
+
+export default create({
+  categories: collection({
+    itemScope: '[data-test-category-item]',
+    item: {
+      open: clickable('[data-test-category-show-link]'),
+      thumbName: text('[data-test-category-name]')
+    }
+  }),
+  visit: visitable('/categories'),
+  categoryName: text('[data-test-category-show-name]'),
+  openNewCategory: clickable('[data-test-categories-new-link]'),
+  openCategories: clickable('[data-test-categories-link]'),
+  edit: clickable('[data-test-category-edit-link]'),
+  editCategoryName: fillable('input', { at: 0 }),
+  editCategoryNameError: text("li:contains('Name can')"),
+  delete: clickable('[data-test-category-destroy-button]'),
+  cancelDelete: clickable('[data-test-cancel-button]'),
+  confirmDelete: clickable('[data-test-confirm-button]'),
+  saveCategory: clickable('[data-test-category-form-save-button]')
+});
