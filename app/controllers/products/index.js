476e4713bc31c16d81f3fe931063e14a5b607bd2
MattSkrobis <skrobismateusz@gmail.com>
2017-08-22 14:29:54 +0200

    Implement a search and a filter

--- app/controllers/products/index.js
+++ app/controllers/products/index.js
@@ -1,14 +1,14 @@
 import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
 
-const { Controller, run } = Ember;
+const { Controller } = Ember;
 
 export default Controller.extend({
   queryParams: ['availability', 'name'],
   availability: true,
   name: null,
-  actions: {
-    debouncedNameSearch() {
-      
-    }
-  }
+  nameUpdate: task(function* (name) {
+    this.set('name', name);
+    yield timeout(250);
+  }).restartable()
 });
