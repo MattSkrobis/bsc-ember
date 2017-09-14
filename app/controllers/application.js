ac40bd8d6495ef3ce0840b49fec25a731ee58fa1
Mateusz Skrobis <skrobis.mateusz@gmail.com>
2017-09-14 10:38:48 +0200

    Lint fix

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
-import ENV from 'bsc-ember/config/environment';
 
-const { Controller, inject: { service }, $  } = Ember;
+const { Controller, inject: { service } } = Ember;
 
 export default Controller.extend({
   session: service('session'),
