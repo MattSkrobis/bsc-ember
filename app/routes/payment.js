27bf369d66c0f9fe366ea44307ec6a1404c35670
Mateusz Skrobis <skrobis.mateusz@gmail.com>
2018-02-03 10:36:56 +0100

    Fix product thumb,

--- app/routes/payment.js
+++ app/routes/payment.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
+const { Route, inject: { service }, computed, RSVP } = Ember;
+
 export default Ember.Route.extend({
+  shoppingCart: service(),
   model(params) {
     
   }
--- app/routes/preferred-products.js
+++ app/routes/preferred-products.js
@@ -4,9 +4,6 @@ const { Route, inject: { service }, computed, RSVP } = Ember;
 
 export default Route.extend({
   currentUser: service(),
-  kot: computed('model.products.[]', function() {
-    return this.get('currentUser.user');
-  }),
   model(params) {
     return RSVP.hash({
       customer: this.store.query('user', {
@@ -18,14 +15,5 @@ export default Route.extend({
         include: 'category'
       })
     });
-  },
-  // afterModel(params) {
-  //   console.log(this.get('currentUser.user'));
-  //   debugger
-  //   this.set('user', this.get('currentUser.user'));
-  //   return this.store.query('user', {
-  //     filter: { id: params.user_id },
-  //     include: 'user-answers.question,user-answers.answer,'
-  //   });
-  // }
+  }
 });
